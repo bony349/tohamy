056b6e9032b937e0fb3f5b10712a248622e83bda
bony349 <58995783+bony349@users.noreply.github.com>
2022-03-04 18:31:39 +0200

    Update helper.py

--- helper.py
+++ helper.py
@@ -12,6 +12,6 @@ from skimage import transform
 
 def Load_Images(img):
   pred_img = np.array(img).astype('float32')/255
-  pred_img = transform.resize(pred_img,(200,200,3))
+  pred_img = transform.resize(pred_img,(128,128,3))
   pred_img = np.expand_dims(pred_img,axis=0)
   return pred_img
